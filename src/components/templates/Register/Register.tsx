87326f14e6dc20fa65cb9e9a3a0e097771325eda
cnvs_linh <nguyenhoangthanhlinh.canvas@gmail.com>
2024-05-08 16:50:56 +0700

    updated

---
+++ src/components/templates/Register/Register.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { Logo } from '@/components/Logo'
+import { LoginForm } from '@/components/organisms/LoginForm'
+
+export const Login = () => {
+  return (
+    <>
+      <div className="flex">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-10 w-auto" />
+        </Link>
+      </div>
+      <h2 className="mt-20 text-lg font-semibold text-gray-900">
+        Get started for free
+      </h2>
+      <p className="mt-2 text-sm text-gray-700">
+        Already registered?{' '}
+        <Link
+          href="/login"
+          className="font-medium text-blue-600 hover:underline"
+        >
+          Sign in
+        </Link>{' '}
+        to your account.
+      </p>
+      <LoginForm />
+    </>
+  )
+}
